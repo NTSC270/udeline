076dabb56fc95d2ccd0b843b08f18cfd63fcb0d4
NTSC270 <60107665+NTSC270@users.noreply.github.com>
2022-02-25 20:41:24 +0100

    prefix code fix

--- helpers/helper.py
+++ helpers/helper.py
@@ -1,12 +1,14 @@
 import json
 
-prefixes = {"udeline": ["u>", "udel>"], "udevline": ["u>>", "udel>>"]}
+prefixes = {"udeline": ["u,", "u>"], "udevline": ["u,,", "u>>"]}
 udel_emoji = {"udeline": "<:udeline:937436511899627620>", "udevline": "<:udevline:940528057830211584>"}
 
 class prefix:
     def check(msg, client):
+        checked = 0
         for x in prefixes[client.user.name]:
             if msg.startswith(x):
                 return [True, len(x), x]
-            else:
-                return [False, 0, ""]
+                checked = 1
+        if checked == 0:
+            return [False, 0, ""]
